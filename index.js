3ad178a3f87e08a5a0a703cb1aa35dc130905385
Jamilson do Nascimento <iero.eng.agricola@gmail.com>
2021-09-29 17:38:48 -0300

    configurado formulario

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express")
 app = express()
 app.set('view engine', 'ejs')
 app.use((express.static('public')))
+app.use(express.urlencoded({extended:true})) // traduz os dados de um formulário recebido  para uma estrutura js
+app.use(express.json()) // ler dados de formulários enviados via json.
 
 
 app.get("/", (req, res)=>{
@@ -12,4 +14,13 @@ app.get("/", (req, res)=>{
 app.get("/perguntar",(req, res)=>{
     res.render("perguntar")
 })
+
+app.post("/salvarpergunta", (req, res)=>{
+    var titulo =req.body.titulo
+    var descricao =req.body.descricao
+
+    res.send("Formulário recebido! titulo: "  + titulo + " " + " descricao: " + descricao)
+
+})
 app.listen(666)
+
